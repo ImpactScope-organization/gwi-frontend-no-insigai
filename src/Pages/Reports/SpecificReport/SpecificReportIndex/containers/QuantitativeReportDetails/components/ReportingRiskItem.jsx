25225f6ebfe577b4b8263594c4c5e498fc1b61b8
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-02-27 16:19:50 +0100

    dom nesting fix

--- src/Pages/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/ReportingRiskItem.jsx
+++ src/Pages/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/ReportingRiskItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ReportingRiskItem = ({ title, children }) => (
   <div className="grid grid-cols-2">
-    <p className="text-reportGrey  text-[1em] text-base mb-1 font-medium">{title}</p>
-    <p className="text-darkBlack  text-[1em] text-base mb-1 font-medium">{children}</p>
+    <h3 className="text-reportGrey  text-[1em] text-base mb-1 font-medium">{title}</h3>
+    <div className="text-darkBlack  text-[1em] text-base mb-1 font-medium">{children}</div>
   </div>
 )
