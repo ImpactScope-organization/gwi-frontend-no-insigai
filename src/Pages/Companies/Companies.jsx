4216bfb819b9f645daa7b84ee9e7495687225c74
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-03-05 14:29:48 +0100

    list companies

--- src/Pages/Companies/Companies.jsx
+++ src/Pages/Companies/Companies.jsx
@@ -1,3 +1,18 @@
+import { PageHeader } from '../../Components/Page/PageHeader/PageHeader'
+import { ButtonLink } from '../../Components/ButtonLink/ButtonLink'
+import { ROUTES } from '../../routes'
+import { CategorizedListContainer } from '../../Components/CategorizedList/CategorizedListContainer/CategorizedListContainer'
+import React from 'react'
+import { PageContainer } from '../../Components/Page/PageContainer/PageContainer'
+
 export const Companies = () => {
-  return <div>Companies</div>
+  return (
+    <PageContainer>
+      <PageHeader title="Companies" subTitle="Overview all of companies here">
+        <ButtonLink to={ROUTES.companies.create}>Add new company</ButtonLink>
+      </PageHeader>
+      {/* Reports Container */}
+      <CategorizedListContainer>hello</CategorizedListContainer>
+    </PageContainer>
+  )
 }
