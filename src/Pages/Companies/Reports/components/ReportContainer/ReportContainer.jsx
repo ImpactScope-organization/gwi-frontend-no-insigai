4559b632a1323593716b97aef5750574a04ce16f
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-03-07 11:17:06 +0100

    tab

--- src/Pages/Companies/Reports/components/ReportContainer/ReportContainer.jsx
+++ src/Pages/Companies/Reports/components/ReportContainer/ReportContainer.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { ROUTES } from '../../../../../routes'
+import { getRouteWithParams, ROUTES } from '../../../../../routes'
 import { ButtonLink } from '../../../../../Components/ButtonLink/ButtonLink'
 import { PageContainer } from '../../../../../Components/Page/PageContainer/PageContainer'
 import { PageHeader } from '../../../../../Components/Page/PageHeader/PageHeader'
 import { PageContentContainer } from '../../../../../Components/Page/PageContentContainer/PageContentContainer'
 import { CategorizedListContainer } from '../../../../../Components/CategorizedList/CategorizedListContainer/CategorizedListContainer'
 import { PageTab } from '../../../../../Components/Page/PageTab/PageTab'
+import { useParams } from 'react-router-dom'
 
 export const ReportContainer = ({ children }) => {
+  const { companyId } = useParams()
+
   return (
     <PageContainer>
       <PageHeader title="Companies" subTitle="Overview all of companies here">
@@ -16,7 +19,13 @@ export const ReportContainer = ({ children }) => {
 
       {/* Tabs Container */}
       <PageContentContainer>
-        <PageTab to={ROUTES.reports.internal}>Internal reports</PageTab>
+        <PageTab
+          to={getRouteWithParams(ROUTES.companies.reports.internal, {
+            companyId
+          })}
+        >
+          Internal reports
+        </PageTab>
         <PageTab to={ROUTES.reports.regulator}>Sent to regulator</PageTab>
         <PageTab to={ROUTES.reports.processing}>Processing reports</PageTab>
       </PageContentContainer>
