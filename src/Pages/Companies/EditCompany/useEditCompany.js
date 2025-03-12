3bce7631a864cc1a80562c88b5cb164b7e14b8d7
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-03-12 11:19:28 +0100

    add metadata

--- src/Pages/Companies/EditCompany/useEditCompany.js
+++ src/Pages/Companies/EditCompany/useEditCompany.js
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { useCallback } from 'react'
@@ -15,11 +15,21 @@ export const useEditCompany = () => {
   const editCompanyFormik = useFormik({
     initialValues: {
       name: '',
-      companyId: ''
+      companyId: '',
+      jurisdiction: '',
+      sector: '',
+      annualRevenue: '',
+      noOfEmployees: '',
+      GHGEmissions: ''
     },
     validationSchema: Yup.object({
       name: Yup.string().required(),
-      companyId: Yup.string().required()
+      companyId: Yup.string().required(),
+      jurisdiction: Yup.string().required(),
+      sector: Yup.string().required(),
+      annualRevenue: Yup.string().required(),
+      noOfEmployees: Yup.string().required(),
+      GHGEmissions: Yup.string().required()
     }),
     onSubmit: async (values) => {
       await handleCreateCompany(values)
