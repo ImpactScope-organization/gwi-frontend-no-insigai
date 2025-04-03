67300985ee2500f17ca6fa886bdd6860f9b03ab7
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-03 14:10:55 +0200

    style

--- src/Pages/Clients/EditClient/EditClient.jsx
+++ src/Pages/Clients/EditClient/EditClient.jsx
@@ -3,15 +3,12 @@ import { ROUTES } from '../../../routes'
 import React from 'react'
 import { Divider } from 'antd'
 import { EditClientForm } from './components/EditClientForm/EditClientForm'
-import { useGetClient } from '../api/ClientApi/ClientApiQuery'
 import { CreateClientUserForm } from './components/CreateClientUserForm/CreateClientUserForm'
 import { ClientUserList } from './components/ClientUserList/ClientUserList'
 
 export const EditClient = () => {
-  const { client } = useGetClient()
-
   return (
-    <TitleWithBackButton title={client?.name} to={ROUTES.clients.index}>
+    <TitleWithBackButton to={ROUTES.clients.index}>
       <EditClientForm />
       <Divider className="my-8" />
       <CreateClientUserForm />
