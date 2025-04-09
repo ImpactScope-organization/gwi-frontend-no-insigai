ba0f379be5bdca58e6522c7e9a8bf7709081e4a5
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-09 10:52:17 +0200

    remove mb4

--- src/Pages/Clients/EditClient/components/CreateClientUserForm/CreateClientUserForm.jsx
+++ src/Pages/Clients/EditClient/components/CreateClientUserForm/CreateClientUserForm.jsx
@@ -15,7 +15,7 @@ export const CreateClientUserForm = () => {
       <Form>
         <div className="flex flex-col w-full gap-4">
           <FormHeading>Create new user for client</FormHeading>
-          <div className="flex flex-row w-full gap-4 mb-4">
+          <div className="flex flex-row w-full gap-4">
             <InputText name="email" label="E-Mail" />
             <InputPassword name="password" label="Password" />
           </div>
