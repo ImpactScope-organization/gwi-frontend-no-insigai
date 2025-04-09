c6b6542e63f2d7eba75648395dc96946653dad85
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-09 10:48:02 +0200

    refact

--- src/Pages/Clients/EditClient/components/ClientUserList/ClientUserListItem/EditClientUserListItem.jsx
+++ src/Pages/Clients/EditClient/components/ClientUserList/ClientUserListItem/EditClientUserListItem.jsx
@@ -1,6 +1,6 @@
+import React from 'react'
 import { InputText } from '../../../../../../Components/Fields/InputText'
 import { InputPassword } from '../../../../../../Components/Fields/InputPassword'
-import React from 'react'
 import { useEditClientUserListItem } from './useEditClientUserListItem'
 import { Form, FormikProvider } from 'formik'
 import { SuccessButton } from '../../../../../../Components/Buttons/SuccessButton'
@@ -10,7 +10,7 @@ export const EditClientUserListItem = ({ clientUser }) => {
   return (
     <FormikProvider value={editClientUserListItemFormik}>
       <Form>
-        <div className="hover:bg-green-100 flex flex-row gap-4 mb-4">
+        <div className="flex flex-row gap-4 mb-4">
           <InputText name="email" label="E-Mail" />
           <InputPassword name="password" label="Password" />
           <InputPassword name="passwordAgain" label="Password Again" />
