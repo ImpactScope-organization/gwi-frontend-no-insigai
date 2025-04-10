bb15318f253b1f581e3af6a2f5f3adaf4a0d453d
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-10 09:49:19 +0200

    fix logic

--- src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportVisibility/components/ClientVisibility/components/ClientItem/useClientItem.js
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportVisibility/components/ClientVisibility/components/ClientItem/useClientItem.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { useCurrentCompanyReport } from '../../../../../../../hooks/useCurrentCompanyReport'
 import axios from 'axios'
 import apiUrl from '../../../../../../../../../../../utils/baseURL'
@@ -5,13 +6,19 @@ import apiUrl from '../../../../../../../../../../../utils/baseURL'
 export const useClientItem = ({ client }) => {
   const { getCurrentCompanyReport, currentCompanyReport, reportId } = useCurrentCompanyReport()
 
+  const getFilteredClientIds = useCallback(
+    (selected) => {
+      if (selected) {
+        return [...currentCompanyReport.clientIds, client.id]
+      } else {
+        return currentCompanyReport.clientIds.filter((id) => id !== client.id)
+      }
+    },
+    [client.id, currentCompanyReport.clientIds]
+  )
+
   const handleClientChange = async (selected) => {
-    const clientIds = [...currentCompanyReport.clientIds]
-    if (selected) {
-      clientIds.push(client.id)
-    } else {
-      clientIds.splice(client.id, 1)
-    }
+    const clientIds = getFilteredClientIds(selected)
 
     await axios.put(`${apiUrl}/api/report/update/${reportId}`, {
       clientIds
