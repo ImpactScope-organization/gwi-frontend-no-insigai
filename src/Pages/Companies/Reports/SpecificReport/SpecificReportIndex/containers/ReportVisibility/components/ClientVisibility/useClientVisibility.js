4f2bab80baea84d2d4cf50c5fbeebb8c4f9a8fb0
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-10 15:32:21 +0200

    memo

--- src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportVisibility/components/ClientVisibility/useClientVisibility.js
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportVisibility/components/ClientVisibility/useClientVisibility.js
@@ -1,16 +1,21 @@
 import { useFetchClientList } from '../../../../../../../../Clients/api/ClientApi/ClientApiQuery'
 import { useCurrentCompanyReport } from '../../../../../hooks/useCurrentCompanyReport'
+import { useMemo } from 'react'
 
 export const useClientVisibility = () => {
   const { data: clients } = useFetchClientList()
   const { currentCompanyReport } = useCurrentCompanyReport()
 
-  const clientToggleList = clients?.map((client) => {
-    return {
-      ...client,
-      isSelected: currentCompanyReport?.clientIds?.includes(client.id) || false
-    }
-  })
+  const clientToggleList = useMemo(
+    () =>
+      clients?.map((client) => {
+        return {
+          ...client,
+          isSelected: currentCompanyReport?.clientIds?.includes(client.id) || false
+        }
+      }),
+    [clients, currentCompanyReport?.clientIds]
+  )
 
   return {
     clientToggleList
