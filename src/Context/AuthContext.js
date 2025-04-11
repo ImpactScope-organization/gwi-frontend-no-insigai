a897cbe5e56dd16e547c683b9242615ed7aa6800
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-11 15:25:43 +0200

    auth methods

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,6 +1,7 @@
-import React, { createContext, useState, useEffect, useContext, useCallback } from 'react'
+import React, { createContext, useState, useEffect, useContext, useCallback, useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../routes'
+import { jwtDecode } from 'jwt-decode'
 
 const AuthContext = createContext({
   isAuthenticated: false,
@@ -8,20 +9,37 @@ const AuthContext = createContext({
   logout: () => {}
 })
 
-const USER_TOKEN_STORAGE_KEY = 'userInfo'
 const ACCESS_TOKEN_STORAGE_KEY = 'accessToken'
 const REFRESH_TOKEN_STORAGE_KEY = 'refreshToken'
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isLocalStorageFetched, setIsLocalStorageFetched] = useState(false)
+  const [roles, setRoles] = useState([])
+  const [availableClientIds, setAvailableClientIds] = useState([])
+
   const navigate = useNavigate()
 
-  const login = useCallback(({ accessToken, refreshToken }) => {
-    localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken)
-    localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken)
-    setIsAuthenticated(true)
+  const loadJWTContent = useCallback((accessToken) => {
+    const { roles: rolesToSet, clientIds: clientIdsToSet } = jwtDecode(accessToken)
+    setRoles(rolesToSet)
+    setAvailableClientIds(clientIdsToSet)
   }, [])
+
+  const clearJWTContent = useCallback(() => {
+    setRoles([])
+    setAvailableClientIds([])
+  }, [])
+
+  const login = useCallback(
+    ({ accessToken, refreshToken }) => {
+      localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken)
+      localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken)
+      setIsAuthenticated(true)
+      loadJWTContent(accessToken)
+    },
+    [loadJWTContent]
+  )
 
   const logout = useCallback(() => {
     localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY)
@@ -33,18 +51,35 @@ export const AuthProvider = ({ children }) => {
     const userInfo = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY)
     if (userInfo) {
       setIsAuthenticated(true)
+      loadJWTContent(userInfo)
     }
     setIsLocalStorageFetched(true)
-  }, [])
+  }, [loadJWTContent])
 
   useEffect(() => {
     if (isLocalStorageFetched && !isAuthenticated) {
       navigate(ROUTES.login)
+      clearJWTContent()
     }
-  }, [isAuthenticated, isLocalStorageFetched, navigate])
+  }, [clearJWTContent, isAuthenticated, isLocalStorageFetched, navigate])
+
+  const userRoles = useMemo(() => {
+    return {
+      isAdmin: roles.includes('admin'),
+      isRegulator: roles.includes('regulator'),
+      isDemo: roles.includes('demo')
+    }
+  }, [roles])
+
+  const isClientAvailable = useCallback(
+    (clientId) => {
+      return availableClientIds.includes(clientId)
+    },
+    [availableClientIds]
+  )
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, userRoles, isClientAvailable }}>
       {children}
     </AuthContext.Provider>
   )
