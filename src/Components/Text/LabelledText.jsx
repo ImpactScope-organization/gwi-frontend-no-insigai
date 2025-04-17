d85662b5fb2d3a970a06b3d4e451852f58b62a04
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-17 12:49:16 +0200

    existing user list item

--- src/Components/Text/LabelledText.jsx
+++ src/Components/Text/LabelledText.jsx
@@ -2,7 +2,7 @@ export const LabelledText = ({ label, children }) => {
   return (
     <div className="flex flex-col w-full">
       <label className="text-md text-darkBlack mb-1 font-semibold block">{label}</label>
-      <div className="py-4">{children}</div>
+      <div className="py-4 w-48 overflow-y-scroll text-sm">{children}</div>
     </div>
   )
 }
