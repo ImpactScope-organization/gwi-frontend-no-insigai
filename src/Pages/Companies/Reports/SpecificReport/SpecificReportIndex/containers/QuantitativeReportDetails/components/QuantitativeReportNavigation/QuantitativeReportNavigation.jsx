45a53796cd71513ff581c324ae9a90557cae47ad
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-17 16:58:11 +0200

    quantitative report details

---
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/QuantitativeReportNavigation/QuantitativeReportNavigation.jsx
@@ -0,0 +1,62 @@
+import { Dropdown } from 'antd'
+import { IoEllipsisHorizontalSharp } from 'react-icons/io5'
+import { captureScreen } from '../../../../../../../../../utils/helpers'
+import React from 'react'
+import { useQuantitativeReportNavigation } from './useQuantitativeReportNavigation'
+import { useCurrentCompanyReport } from '../../../../../hooks/useCurrentCompanyReport'
+
+export const QuantitativeReportNavigation = () => {
+  const { currentCompanyReport } = useCurrentCompanyReport()
+
+  const {
+    handleSendToBlockchain,
+    isSendToBlockchainInProgress,
+    deleteCompanyHandler,
+    dropdownConfiguration,
+    storedOnBlockchain
+  } = useQuantitativeReportNavigation()
+
+  return (
+    <>
+      {!storedOnBlockchain && (
+        <div className="flex flex-row gap-4 w-full">
+          <button
+            disabled={isSendToBlockchainInProgress}
+            onClick={handleSendToBlockchain}
+            className={`${
+              isSendToBlockchainInProgress ? 'bg-greyText' : 'bg-darkGreen'
+            } flex-1 rounded-lg py-3 px-3 border-none outline-none text-[#fff] text-[16px] font-[600] leading-[24px]`}
+          >
+            Send to blockchain
+          </button>
+          <Dropdown
+            disabled={isSendToBlockchainInProgress}
+            trigger={['click']}
+            menu={dropdownConfiguration}
+            placement="bottomRight"
+          >
+            <div className="py-[12px] px-[18px] rounded-md border bg-transparent flex justify-center items-center">
+              <IoEllipsisHorizontalSharp />
+            </div>
+          </Dropdown>
+        </div>
+      )}
+      {storedOnBlockchain && (
+        <div className="flex flex-row justify-center gap-2 col-span-2 w-full">
+          <button
+            onClick={() => captureScreen('report-container', currentCompanyReport?.companyName)}
+            className="bg-primary rounded-lg py-[12px] flex w-full text-center justify-center px-[4px] col-span-1 border-none outline-none text-[#fff] text-[16px] font-[600] leading-[24px]"
+          >
+            Download as .pdf
+          </button>
+          <button
+            onClick={deleteCompanyHandler}
+            className="bg-white border border-darkBlack rounded-lg w-full text-center justify-center flex py-[12px] col-span-1 px-[4px] text-darkBlack text-[16px] font-[600] leading-[24px]"
+          >
+            Remove from DB
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
---
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/QuantitativeReportNavigation/useQuantitativeReportNavigation.js
@@ -0,0 +1,108 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import { useCallback, useMemo, useState } from 'react'
+import html2canvas from 'html2canvas'
+import { toast } from 'react-toastify'
+import { useCurrentCompanyReport } from '../../../../../hooks/useCurrentCompanyReport'
+import { getApi } from '../../../../../../../../../utils/api'
+import { getRouteWithParams, ROUTES } from '../../../../../../../../../routes'
+import { captureScreen } from '../../../../../../../../../utils/helpers'
+
+export const useQuantitativeReportNavigation = () => {
+  const navigate = useNavigate()
+  const { companyId } = useParams()
+  const { getCurrentCompanyReport, currentCompanyReport, reportId } = useCurrentCompanyReport()
+
+  const [isSendToBlockchainInProgress, setIsSendToBlockchainInProgress] = useState(false)
+
+  const storedOnBlockchain =
+    currentCompanyReport?.blockchainFileURL && currentCompanyReport?.blockchainTransactionURL
+
+  const handleSendToBlockchain = useCallback(async () => {
+    setIsSendToBlockchainInProgress(true)
+    try {
+      const element = document.querySelector('#report-container')
+
+      const canvas = await html2canvas(element)
+      const blob = await new Promise((resolve) => canvas.toBlob(resolve, 'image/jpeg'))
+
+      const file = new File([blob], 'fileName.jpg', { type: 'image/jpeg' })
+      const formData = new FormData()
+      formData.append('file', file)
+
+      await (
+        await getApi()
+      ).post(`/api/blockchain/create/${reportId}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Accept: '*/*',
+          'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+        }
+      })
+      await getCurrentCompanyReport()
+    } catch (error) {
+      if (error?.response?.data?.message) {
+        toast.error(error?.response?.data?.message)
+      } else {
+        toast(error.message)
+      }
+    } finally {
+      setIsSendToBlockchainInProgress(false)
+    }
+  }, [getCurrentCompanyReport, reportId])
+
+  const deleteCompanyHandler = useCallback(async () => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete this Report? \n${currentCompanyReport?.companyName}`
+      )
+    ) {
+      const response = await (
+        await getApi()
+      ).delete(`/api/report/delete/${currentCompanyReport?.id}`)
+      const { data } = response
+      if (data?.status === 'success') {
+        toast.success(data?.message)
+        navigate(
+          getRouteWithParams(ROUTES.companies.reports.internal, {
+            companyId
+          })
+        )
+      } else {
+        toast.error('something went wrong while deleting the report')
+      }
+    }
+  }, [companyId, currentCompanyReport, navigate])
+
+  const dropdownConfiguration = useMemo(() => {
+    return {
+      items: [
+        {
+          label: 'Modify Report',
+          onClick: () => {
+            navigate(
+              getRouteWithParams(ROUTES.companies.reports.report.edit, {
+                companyId,
+                reportId
+              })
+            )
+          }
+        },
+        {
+          label: 'Save as PDF',
+          onClick: () => {
+            captureScreen('report-container', currentCompanyReport?.companyName)
+          }
+        },
+        { label: 'Remove from DB', onClick: deleteCompanyHandler }
+      ]
+    }
+  }, [companyId, currentCompanyReport?.companyName, deleteCompanyHandler, navigate, reportId])
+
+  return {
+    handleSendToBlockchain,
+    isSendToBlockchainInProgress,
+    deleteCompanyHandler,
+    dropdownConfiguration,
+    storedOnBlockchain
+  }
+}
