73ff7ad6be769d51cc302b6f3fce1c14d789dad9
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-04-23 14:35:52 +0200

    refresh tokens

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import apiUrl from './baseURL'
-import { ACCESS_TOKEN_STORAGE_KEY } from './auth'
+import { ACCESS_TOKEN_STORAGE_KEY, REFRESH_TOKEN_STORAGE_KEY } from './auth'
 
 const ApiInstance = axios.create({
   baseURL: apiUrl,
@@ -19,6 +19,41 @@ export async function getApi() {
 
     return config
   })
+
+  ApiInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config
+
+      if (error.response?.status === 500 && !originalRequest._retry) {
+        originalRequest._retry = true
+
+        try {
+          const refreshTokenToUse = localStorage.getItem(REFRESH_TOKEN_STORAGE_KEY)
+          if (refreshTokenToUse) {
+            const {
+              data: {
+                result: { accessToken, refreshToken }
+              }
+            } = await axios.post(`${apiUrl}/api/auth/refresh`, {
+              refreshToken: refreshTokenToUse
+            })
+
+            localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken)
+            localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken)
+
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`
+            return ApiInstance(originalRequest)
+          }
+        } catch (refreshError) {
+          console.error('Failed to refresh token:', refreshError)
+          return Promise.reject(refreshError)
+        }
+      }
+
+      return Promise.reject(error)
+    }
+  )
 
   return ApiInstance
 }
