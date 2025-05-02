0bafb06393497dc18d3a11cfa486fc44483a2ce1
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-02 16:34:26 +0200

    fix gap

--- src/Components/TitleWithBackButton/TitleWithBackButton.jsx
+++ src/Components/TitleWithBackButton/TitleWithBackButton.jsx
@@ -4,7 +4,7 @@ import { BackButtonLink } from '../BackButtonLink/BackButtonLink'
 
 export const TitleWithBackButton = ({ title, to, children }) => (
   <PageContainer className="pb-10">
-    <div className="flex items-top w-full gap-8">
+    <div className="flex items-top w-full gap-8 mb-8">
       <BackButtonLink to={to} />
       <h2 className="text-darkBlack font-bold text-3xl">{title}</h2>
     </div>
