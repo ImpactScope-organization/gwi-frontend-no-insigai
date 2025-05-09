52db414fedf387be87a6b304c362f94fe222541d
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-09 15:24:15 +0200

    select input

--- src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/components/CompanyDocumentInput/CompanyDocumentInput.jsx
+++ src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/components/CompanyDocumentInput/CompanyDocumentInput.jsx
@@ -2,6 +2,7 @@ import { useGetCompanyDocuments } from '../../../../../api/CompanyApiQuery'
 import { Select } from 'antd'
 import { useMemo, useState } from 'react'
 import { CategorySelectGroupTitle } from '../../../../../../Prompts/components/CategorySelect/components/CategorySelectGroupTitle'
+import { SuccessButton } from '../../../../../../../Components/Buttons/SuccessButton'
 
 export const CompanyDocumentInput = () => {
   const { companyDocuments } = useGetCompanyDocuments()
@@ -13,35 +14,44 @@ export const CompanyDocumentInput = () => {
   )
 
   return (
-    <div>
-      <CategorySelectGroupTitle>Documents</CategorySelectGroupTitle>
-      <div className="w-full flex gap-4">
-        <Select
-          className="w-full"
-          onChange={(yearToSet) => {
-            setYear(yearToSet)
-          }}
-        >
-          {companyDocuments &&
-            companyDocuments.map(({ year }) => (
-              <Select.Option key={`company_document_${year}`} value={year}>
-                {year}
-              </Select.Option>
-            ))}
-        </Select>
-        <Select
-          className="w-full"
-          onChange={(yearToSet) => {
-            setYear(yearToSet)
-          }}
-        >
-          {yearDocuments &&
-            yearDocuments.map(({ documentId, title }) => (
-              <Select.Option key={documentId} value={documentId}>
-                {title}
-              </Select.Option>
-            ))}
-        </Select>
+    <div className="flex flex-col w-full gap-4">
+      <div>
+        <CategorySelectGroupTitle>Select Documents</CategorySelectGroupTitle>
+        <div className="w-full flex gap-4">
+          <Select
+            className="w-full"
+            onChange={(yearToSet) => {
+              setYear(yearToSet)
+            }}
+          >
+            {companyDocuments &&
+              companyDocuments.map(({ year }) => (
+                <Select.Option key={`company_document_${year}`} value={year}>
+                  {year}
+                </Select.Option>
+              ))}
+          </Select>
+          <Select
+            className="w-full"
+            onChange={(yearToSet) => {
+              setYear(yearToSet)
+            }}
+          >
+            {yearDocuments &&
+              yearDocuments.map(({ documentId, title }) => (
+                <Select.Option key={documentId} value={documentId}>
+                  {title}
+                </Select.Option>
+              ))}
+          </Select>
+          <div>
+            <SuccessButton>Add</SuccessButton>
+          </div>
+        </div>
+      </div>
+      <div>
+        <CategorySelectGroupTitle>Documents</CategorySelectGroupTitle>
+        <div>No documents selected</div>
       </div>
     </div>
   )
