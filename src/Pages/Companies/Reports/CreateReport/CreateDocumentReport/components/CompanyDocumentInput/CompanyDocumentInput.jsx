6c8c888ef428e3daa059eb332eaa0ee764474b50
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-09 15:17:11 +0200

    document selector

--- src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/components/CompanyDocumentInput/CompanyDocumentInput.jsx
+++ src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/components/CompanyDocumentInput/CompanyDocumentInput.jsx
@@ -1,7 +1,48 @@
 import { useGetCompanyDocuments } from '../../../../../api/CompanyApiQuery'
+import { Select } from 'antd'
+import { useMemo, useState } from 'react'
+import { CategorySelectGroupTitle } from '../../../../../../Prompts/components/CategorySelect/components/CategorySelectGroupTitle'
 
 export const CompanyDocumentInput = () => {
   const { companyDocuments } = useGetCompanyDocuments()
+  const [year, setYear] = useState()
 
-  return <div>CompanyDocumentInput</div>
+  const yearDocuments = useMemo(
+    () => companyDocuments?.find((document) => document.year === year)?.documents,
+    [companyDocuments, year]
+  )
+
+  return (
+    <div>
+      <CategorySelectGroupTitle>Documents</CategorySelectGroupTitle>
+      <div className="w-full flex gap-4">
+        <Select
+          className="w-full"
+          onChange={(yearToSet) => {
+            setYear(yearToSet)
+          }}
+        >
+          {companyDocuments &&
+            companyDocuments.map(({ year }) => (
+              <Select.Option key={`company_document_${year}`} value={year}>
+                {year}
+              </Select.Option>
+            ))}
+        </Select>
+        <Select
+          className="w-full"
+          onChange={(yearToSet) => {
+            setYear(yearToSet)
+          }}
+        >
+          {yearDocuments &&
+            yearDocuments.map(({ documentId, title }) => (
+              <Select.Option key={documentId} value={documentId}>
+                {title}
+              </Select.Option>
+            ))}
+        </Select>
+      </div>
+    </div>
+  )
 }
