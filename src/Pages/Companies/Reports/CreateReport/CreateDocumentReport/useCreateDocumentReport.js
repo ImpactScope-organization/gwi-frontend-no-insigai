6d50457fc76f7e42e14a5b664566a715a42ad008
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-13 11:55:14 +0200

    register

--- src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/useCreateDocumentReport.js
+++ src/Pages/Companies/Reports/CreateReport/CreateDocumentReport/useCreateDocumentReport.js
@@ -2,11 +2,14 @@ import { useGetCompanyDocuments } from '../../../api/CompanyApiQuery'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { createDocumentReportQueueItem } from '../../api/ReportQueueApi'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { getRouteWithParams, ROUTES } from '../../../../../routes'
 
 export const useCreateDocumentReport = () => {
   const { companyDocuments } = useGetCompanyDocuments()
   const { companyId } = useParams()
+  const navigate = useNavigate()
 
   const createDocumentReportFormik = useFormik({
     initialValues: {
@@ -18,10 +21,20 @@ export const useCreateDocumentReport = () => {
       documents: Yup.array().min(1).required()
     }),
     async onSubmit(values) {
-      await createDocumentReportQueueItem({
+      const {
+        result: { id }
+      } = await createDocumentReportQueueItem({
         ...values,
         companyId
       })
+
+      toast.success('Report saved successfully')
+      navigate(
+        getRouteWithParams(ROUTES.companies.reports.processingDetails, {
+          companyId,
+          reportQueueId: id
+        })
+      )
     }
   })
 
