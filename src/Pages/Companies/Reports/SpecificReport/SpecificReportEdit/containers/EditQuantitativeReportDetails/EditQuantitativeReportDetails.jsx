fe50f7a40d70a2619a3550ee47807c9bb2bd8294
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-21 11:57:47 +0200

    render legacy

--- src/Pages/Companies/Reports/SpecificReport/SpecificReportEdit/containers/EditQuantitativeReportDetails/EditQuantitativeReportDetails.jsx
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportEdit/containers/EditQuantitativeReportDetails/EditQuantitativeReportDetails.jsx
@@ -1,13 +1,23 @@
 import { SpecificReportInputPercentage } from '../../components/SpecificReportInputText/SpecificReportInputPercentage'
 import { SpecificReportInputText } from '../../components/SpecificReportInputText/SpecificReportInputText'
+import { useCurrentCompanyReport } from '../../../hooks/useCurrentCompanyReport'
 
 export const EditQuantitativeReportDetails = () => {
+  const { currentCompanyReport } = useCurrentCompanyReport()
+
   return (
     <div className="card_shadow rounded-2xl flex basis-4/12 flex-col gap-1 py-4 px-3">
       <h5 className="text-[18px] leading-[24px] font-[600]">Report</h5>
       <div className="flex flex-col gap-[16px] my-[24px]">
-        <SpecificReportInputPercentage name="greenwashRiskPercentage" label="Greenwashing risk" />
-        <SpecificReportInputPercentage name="reportingRiskPercentage" label="Reporting risk" />
+        {currentCompanyReport?.quantitativePercentages?.length === 0 && (
+          <>
+            <SpecificReportInputPercentage
+              name="greenwashRiskPercentage"
+              label="Greenwashing risk"
+            />
+            <SpecificReportInputPercentage name="reportingRiskPercentage" label="Reporting risk" />
+          </>
+        )}
 
         <SpecificReportInputText name="GHGEmissions" label="GHG emissions" />
       </div>
