71ab1388d26b372dcb8cc1f8ce3890b9b8b7f176
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-21 12:39:13 +0200

    handle percentage

--- src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/QuantitativeReportDetailListItem.jsx
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/QuantitativeReportDetails/components/QuantitativeReportDetailListItem.jsx
@@ -1,15 +1,20 @@
 import { ReportDetailsCard } from '../../../../components/ReportDetailsCard/ReportDetailsCard'
 import { GaugeChart } from '../../../components/GaugeChart/GaugeChart'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ReportingRiskBarChart } from '../../../components/ReportingRiskBarChart/ReportingRiskBarChart'
 import { Divider } from 'antd'
 
 export const QuantitativeReportDetailListItem = ({ quantitativePercentage }) => {
+  const percentage = useMemo(
+    () => parseInt(quantitativePercentage.components.reduce((acc, item) => acc + item.value, 0)),
+    [quantitativePercentage]
+  )
+
   return (
     <ReportDetailsCard title={quantitativePercentage.name}>
       <div className="flex flex-col gap-6">
         <div className="w-full px-2 flex justify-center items-center">
-          <GaugeChart percentage={parseInt(quantitativePercentage.value)} />
+          <GaugeChart percentage={percentage} />
         </div>
 
         <Divider className="m-0" />
