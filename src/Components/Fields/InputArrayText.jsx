270c9bd268e49c8ffe99751416833bef8e5a1884
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-05-26 15:45:16 +0200

    Commit message

--- src/Components/Fields/InputArrayText.jsx
+++ src/Components/Fields/InputArrayText.jsx
@@ -40,7 +40,7 @@ export const InputArrayText = ({ name, label, disabled = false }) => {
           status={formik.touched[name] && formik.errors[name] ? 'error' : 'success'}
           disabled={disabled}
         />
-        <SuccessButton type="button" onClick={addItem} rootClassName="absolute top-0 right-0 w-24">
+        <SuccessButton type="button" onClick={addItem} rootClassName="absolute top-0 right-0 w-20">
           Add
         </SuccessButton>
       </div>
