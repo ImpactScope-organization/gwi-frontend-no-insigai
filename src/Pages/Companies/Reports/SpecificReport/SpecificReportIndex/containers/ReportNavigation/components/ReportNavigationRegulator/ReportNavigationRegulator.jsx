b212df57fb83a2d9c8c9ec3333c71ff0a3ede24d
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-06-10 15:00:43 +0200

    rename

--- src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportNavigation/components/ReportNavigationRegulator/ReportNavigationRegulator.jsx
+++ src/Pages/Companies/Reports/SpecificReport/SpecificReportIndex/containers/ReportNavigation/components/ReportNavigationRegulator/ReportNavigationRegulator.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 export const ReportNavigationRegulator = () => {
   const { currentCompanyReport } = useCurrentCompanyReport()
-  const isDownloadAvailable = false
+  const isRegulatorDownloadAvailable = false
 
   return (
     <div className="flex flex-row justify-center gap-2 col-span-2 w-full">
-      {isDownloadAvailable && (
+      {isRegulatorDownloadAvailable && (
         <button
           onClick={() => captureScreen('report-container', currentCompanyReport?.companyName)}
           className="bg-primary rounded-lg py-[12px] flex w-full text-center justify-center px-[4px] col-span-1 border-none outline-none text-[#fff] text-[16px] font-[600] leading-[24px]"
