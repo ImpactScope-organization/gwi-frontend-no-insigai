8ad5b48aad2546c960be6b44e2dcaed1c0c69486
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-06-11 16:42:40 +0200

    filtered prompts

--- src/Pages/Prompts/usePrompts.js
+++ src/Pages/Prompts/usePrompts.js
@@ -1,18 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAllPrompts } from './api/PromptApi'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+
+const FILTER_VALUES = {
+  all: 'all',
+  default: 'default'
+}
+
+const filterOptions = [
+  { label: 'All', value: FILTER_VALUES.all },
+  { label: 'Default', value: FILTER_VALUES.default }
+]
 
 export const usePrompts = () => {
-  const FILTER_VALUES = {
-    all: 'all',
-    default: 'default'
-  }
-
-  const filterOptions = [
-    { label: 'All', value: FILTER_VALUES.all },
-    { label: 'Default', value: FILTER_VALUES.default }
-  ]
-
   const [filter, setFilter] = useState(FILTER_VALUES.all)
 
   const { data: prompts } = useQuery({
@@ -21,15 +21,19 @@ export const usePrompts = () => {
     initialData: []
   })
 
-  const filteredPrompts = prompts.filter((prompt) => {
-    if (filter === FILTER_VALUES.all) {
-      return true
-    }
-    if (filter === FILTER_VALUES.default) {
-      return prompt.isDefault
-    }
-    return false
-  })
+  const filteredPrompts = useMemo(
+    () =>
+      prompts.filter((prompt) => {
+        if (filter === FILTER_VALUES.all) {
+          return true
+        }
+        if (filter === FILTER_VALUES.default) {
+          return prompt.isDefault
+        }
+        return false
+      }),
+    [filter, prompts]
+  )
 
   return {
     filteredPrompts,
