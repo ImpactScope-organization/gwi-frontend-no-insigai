103d51d9bb34dd24f937122baf44478943cef4ab
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-06-11 16:41:34 +0200

    add frontend filtering

--- src/Pages/Prompts/usePrompts.js
+++ src/Pages/Prompts/usePrompts.js
@@ -1,14 +1,40 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAllPrompts } from './api/PromptApi'
+import { useState } from 'react'
 
 export const usePrompts = () => {
+  const FILTER_VALUES = {
+    all: 'all',
+    default: 'default'
+  }
+
+  const filterOptions = [
+    { label: 'All', value: FILTER_VALUES.all },
+    { label: 'Default', value: FILTER_VALUES.default }
+  ]
+
+  const [filter, setFilter] = useState(FILTER_VALUES.all)
+
   const { data: prompts } = useQuery({
     queryKey: ['getAllPrompts'],
     queryFn: () => getAllPrompts(),
     initialData: []
   })
 
+  const filteredPrompts = prompts.filter((prompt) => {
+    if (filter === FILTER_VALUES.all) {
+      return true
+    }
+    if (filter === FILTER_VALUES.default) {
+      return prompt.isDefault
+    }
+    return false
+  })
+
   return {
-    prompts
+    filteredPrompts,
+    filterOptions,
+    filter,
+    setFilter
   }
 }
