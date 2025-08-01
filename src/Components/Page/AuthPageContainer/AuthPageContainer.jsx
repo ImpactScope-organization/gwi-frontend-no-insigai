d76eefd7ffcb4eef4e799ee9478f50bd58dee6a0
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-08-01 17:16:52 +0200

    refactor

--- src/Components/Page/AuthPageContainer/AuthPageContainer.jsx
+++ src/Components/Page/AuthPageContainer/AuthPageContainer.jsx
@@ -4,44 +4,39 @@ export const AuthPageContainer = ({ children }) => (
       <div className="hidden md:flex  flex-col bg-[#E3F2DA] w-1/2 gap-4 p-4 ">
         <div className="flex flex-col gap-6 lg:flex-row w-full justify-between h-12">
           <div className="flex gap-x-4">
-            <div
+            <a
               className="flex justify-center items-center px-1 cursor-pointer"
-              onClick={() => {
-                window.open('https://astar.network/solutions', '_blank')
-              }}
+              href="https://astar.network/solutions"
+              target="_blank"
+              rel="noreferrer"
             >
-              <img src="./assets/GFIN.svg" alt="logo" className=" " />
-            </div>
+              <img src="./assets/GFIN.svg" alt="logo" />
+            </a>
 
-            <div
+            <a
               className="flex justify-center items-center px-1 cursor-pointer"
-              onClick={() => {
-                window.open('https://astar.network/solutions', '_blank')
-              }}
+              href="https://astar.network/solutions"
+              target="_blank"
+              rel="noreferrer"
             >
-              <img src="./assets/AstrLogo.svg" alt="logo" className=" " />
-            </div>
+              <img src="./assets/AstrLogo.svg" alt="logo" />
+            </a>
           </div>
-          <div
+          <a
             className="flex flex-col w-32 lg:w-auto cursor-pointer"
-            onClick={() => {
-              window.open('https://www.insg.ai', '_blank')
-            }}
+            href="https://www.insg.ai"
+            target="_blank"
+            rel="noreferrer"
           >
-            <img src="./assets/AILogo.svg" alt="logo" className=" " />
-          </div>
+            <img src="./assets/AILogo.svg" alt="logo" />
+          </a>
         </div>
         <div className="flex  w-full h-full p-20">
-          <img src="./assets/login_sideImage.png" alt="logo" className="" />
+          <img src="./assets/login_sideImage.png" alt="logo" />
         </div>
       </div>
       <div className="flex justify-center items-center w-full md:w-1/2 h-full">
-        <div
-          //   onSubmit={handleSubmit}
-          className="w-[70%] "
-        >
-          {children}
-        </div>
+        <div className="w-[70%] ">{children}</div>
       </div>
     </div>
   </div>
