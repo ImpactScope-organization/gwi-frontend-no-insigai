9bb71bde1744aab7174009dc78bfbd161a1c281b
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-08-25 15:09:20 +0200

    send verification

--- src/Pages/VerifyEmail/useVerifyEmail.js
+++ src/Pages/VerifyEmail/useVerifyEmail.js
@@ -1,17 +1,42 @@
 import { useLoading } from '../../Hooks/useLoading'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { getApi } from '../../utils/api'
 import { useQueryParams } from '../../Hooks/useQueryParams'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../../routes'
 import { toast } from 'react-toastify'
 import { useAuthContext } from '../../Context/AuthContext'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export const useVerifyEmail = () => {
   const { isLoading, startLoading, finishLoading } = useLoading()
   const { queryParams } = useQueryParams()
   const navigate = useNavigate()
   const { login } = useAuthContext()
+
+  const [isResend, setIsResend] = useState(false)
+
+  const sendVerifyEmailFormik = useFormik({
+    initialValues: {
+      email: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Email is required')
+    }),
+    onSubmit: async (values) => {
+      try {
+        startLoading()
+        await (await getApi()).post(`/api/user/send-verification-email`, values)
+        toast.success('Email verification email sent. Please check your inbox.')
+        navigate(ROUTES.login)
+      } catch (err) {
+        toast.error(err?.response?.data?.message)
+      } finally {
+        finishLoading()
+      }
+    }
+  })
 
   const verifyEmail = useCallback(async () => {
     try {
@@ -28,18 +53,19 @@ export const useVerifyEmail = () => {
       navigate(ROUTES.companies.index)
     } catch (err) {
       toast.error('Invalid or expired token.')
-      // todo ask a new token?
-      navigate(ROUTES.login)
+      setIsResend(true)
     } finally {
       finishLoading()
     }
-  }, [finishLoading, navigate, queryParams, startLoading])
+  }, [finishLoading, login, navigate, queryParams, startLoading])
 
   useEffect(() => {
     verifyEmail()
   }, [verifyEmail])
 
   return {
-    isLoading
+    isLoading,
+    isResend,
+    sendVerifyEmailFormik
   }
 }
