ca5de3c3ce7ff3ff074452f12b18822213053f3f
Tahta D. Timur <tahta@impactscope.com>
2025-10-23 11:31:11 +0700

    feat: removed subscription banner from companies and reports pages

--- src/Components/Page/PageHeader/PageHeader.jsx
+++ src/Components/Page/PageHeader/PageHeader.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const PageHeader = ({ title, subTitle, children }) => (
-  <div className="flex justify-between items-start mb-6">
+  <div className="flex justify-between items-center mb-6">
     {/* Left */}
     <div>
       <h1 className="text-darkBlack font-bold text-3xl mb-1">{title}</h1>
       <p className="subtitle-text ">{subTitle}</p>
     </div>
     {/* Right */}
-    {children}
+    <div className="self-center">{children}</div>
   </div>
 )
